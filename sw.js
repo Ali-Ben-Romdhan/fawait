241af5d62e37ea5bf0c26672523789bf45d30750
Ali-Ben-Romdhane <alibenromdhane.bnr@gmail.com>
2026-02-21 12:11:21 +0100

    Add PWA support: manifest, service worker, icons, meta tags

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'fawait-v1';
+const ASSETS = [
+  '/fawait/',
+  '/fawait/index.html',
+  '/fawait/manifest.json',
+  '/fawait/icons/icon-192.png',
+  '/fawait/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
